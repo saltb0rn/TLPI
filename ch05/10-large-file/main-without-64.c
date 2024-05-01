4654971dbc1846bca6674ec2619b02a3c8d91139
Saltb0rn <asche34@outlook.com>
2024-05-01 17:50:31 +0800

    add another lfs example

---
+++ ch05/10-large-file/main-without-64.c
@@ -0,0 +1,30 @@
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tlpi_hdr.h"
+
+int main(int argc, char *argv[]) {
+  int fd;
+  off_t off;
+  if (argc != 3 || strcmp(argv[1], "--help") == 0) {
+    usageErr("Usage: %s PATHNAME OFFSET\n", argv[0]);
+  }
+
+  fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+  if (-1 == fd) {
+    errExit("open64");
+  }
+
+  off = atoll(argv[2]);
+  if (lseek(fd, off, SEEK_SET) == -1) {
+    errExit("lseek64");
+  }
+
+  if (write(fd, "test", 4) == -1) {
+    errExit("write");
+  }
+
+  return EXIT_SUCCESS;
+}
